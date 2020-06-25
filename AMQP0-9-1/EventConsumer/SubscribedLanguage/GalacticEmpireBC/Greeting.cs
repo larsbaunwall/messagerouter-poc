708dd9c5d71796c1f6fabaad29a6239ed980d1e3
Lars Baunwall <larslb@thinkability.dk>
2020-06-26 00:06:42 +0200

    Add json schema attributes to messages

--- AMQP0-9-1/EventConsumer/SubscribedLanguage/GalacticEmpireBC/Greeting.cs
+++ AMQP0-9-1/EventConsumer/SubscribedLanguage/GalacticEmpireBC/Greeting.cs
@@ -1,5 +1,9 @@
-﻿namespace EventConsumer.SubscribedLanguage.GalacticEmpireBC
+﻿using Messaging.Support;
+using Messaging.Support.Validation;
+
+namespace EventConsumer.SubscribedLanguage.GalacticEmpireBC
 {
+    [JsonSchema("https://raw.githubusercontent.com/larsbaunwall/messagerouter-sample/master/schemas/bounded-context/galactic-empire/events.greeting.json")]
     public class Greeting
     {
         public string Sender { get; set; }
--- AMQP0-9-1/EventProducer/PublishedLanguage/Greeting.cs
+++ AMQP0-9-1/EventProducer/PublishedLanguage/Greeting.cs
@@ -1,5 +1,8 @@
+using Messaging.Support.Validation;
+
 namespace EventProducer.PublishedLanguage
 {
+    [JsonSchema("https://raw.githubusercontent.com/larsbaunwall/messagerouter-sample/master/schemas/bounded-context/galactic-empire/events.greeting.json")]
     public class Greeting
     {
         public string Sender { get; set; }

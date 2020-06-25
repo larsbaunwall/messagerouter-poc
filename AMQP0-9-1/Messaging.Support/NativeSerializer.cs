30160109710ee0037dd894680b5fbc752139df01
Lars Baunwall <larslb@thinkability.dk>
2020-06-26 00:05:03 +0200

    Add a native serializer that uses System.Text.Json instead of Newtonsoft.Json

---
+++ AMQP0-9-1/Messaging.Support/NativeSerializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.Json;
+using EasyNetQ;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace Messaging.Support
+{
+    public class NativeSerializer : ISerializer
+    {
+        public byte[] MessageToBytes(Type messageType, object message)
+        {
+            return JsonSerializer.SerializeToUtf8Bytes(message, messageType, new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            });
+        }
+
+        public object BytesToMessage(Type messageType, byte[] bytes)
+        {
+            return JsonSerializer.Deserialize(bytes, messageType);
+        }
+    }
+}
